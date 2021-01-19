7dd9880b379dfae7aa1262df5b1e90019effc2ca
Sergiu Avram <contact@sergiuavram.com>
2021-01-19 20:30:03 +0200

    v0.6
    baseUrl for api request is selected based on hostname (api.shaqodoon or zimbor)

--- src/axios/baseUrl.js
+++ src/axios/baseUrl.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const hostname = window.location.hostname;
+
 export default axios.create({
-    baseURL: 'http://zimbor.go.ro/api/bot'
+    baseURL: hostname.includes('shaqodoon') ? 'https://api.shaqodoon.ro/' : 'http://zimbor.go.ro/api/bot'
+    // baseURL: 'http://zimbor.go.ro/api/bot'
     // baseURL: 'https://api.shaqodoon.ro/'
 })
