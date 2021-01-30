b8c333f14cba55c1914de300884a9e5318b6be02
Sergiu Avram <contact@sergiuavram.com>
2021-01-30 08:17:01 +0200

    v0.18
    added visual cue when hover the job name (it changes color from  #504F4F --> #1E6C93)
    
    changed menu buttons style and added visua cue when hover

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,7 +11,7 @@ const Header = ({ setIsLoading }) => {
 
     return (
         <div className="menu">
-            <Link to='/jobs' jobs={'allJobs'} title='some title' onClick={() => setIsLoading(true)}>Locuri de Muncă</Link>
+            <Link to='/jobs' jobs={'allJobs'} title='some title' onClick={() => setIsLoading(true)}>Toate Locurile de Muncă</Link>
             <Link to='/companies' onClick={() => setIsLoading(true)}>Companii</Link>
         </div>
     );
