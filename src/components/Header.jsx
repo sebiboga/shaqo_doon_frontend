c28cec058390d877027259cd91c3b1fdb7b7d2d5
Sergiu Avram <contact@sergiuavram.com>
2021-01-30 19:24:45 +0200

    v0.19
    solved issue #12 from github/testing
    
    and I added a new bug with scrolling, I am working on it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.scss';
 
 import logo from '../assets/images/shaqo doon logo.png';
@@ -10,14 +10,28 @@ import { getTotal } from '../redux/total/total.actions';
 import { Link } from 'react-router-dom';
 
 const Header = ({ getTotal, total }) => {
+    const [headerHeight, seteHeaderHeight] = useState('280px');
+
+    const onScroll = () => {
+        window.onscroll = () => {
+            if (document.body.scrollTop > 50
+                || document.documentElement.scrollTop > 50
+            ) {
+                seteHeaderHeight('100px');
+            } else {
+                seteHeaderHeight('280px');
+            }
+        }
+    }
 
     useEffect(() => {
         getTotal();
+        onScroll()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
-        <div className="header">
+        <div className="header" style={{ height: headerHeight }}>
             <Link to='/'>
                 <img src={logo} alt="shaqo doon logo" />
             </Link>
