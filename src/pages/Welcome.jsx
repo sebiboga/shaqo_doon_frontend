938bcad7f865da44a281b37e0009ae529f63bca1
Sergiu Avram <contact@sergiuavram.com>
2021-02-07 20:16:15 +0200

    v0.30
    first try home page message + design

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,6 +6,13 @@ const Welcome = () => {
     return (
         <div className="welcome">
             <h1>Buna venit pe <strong>shaqo doon!</strong></h1>
+            <div className="message">
+                <h2>Cauti un loc de munca?</h2>
+                <div className="mission">
+                    <h3>Esti in locul potrivit!</h3>
+                    <h3>Misiunea noastra este<br />sa agregam toate locurile de munca inturn singur loc<br />pentru a te ajuta sa gasesti mai usor locul de munca ideal pentru tine!</h3>
+                </div>
+            </div>
         </div >
     );
 };
