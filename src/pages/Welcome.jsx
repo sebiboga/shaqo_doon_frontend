28c75efdb7df7862f098c799f73745cc5d97a08d
Sergiu Avram <contact@sergiuavram.com>
2021-02-08 17:18:31 +0200

    v0.34
    update homepage with diacritics

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,12 +6,12 @@ const Welcome = () => {
     return (
         <div className="welcome">
             <div className="spacer"></div>
-            <h1>Buna venit pe <strong>shaqo doon!</strong></h1>
+            <h1>Bun venit pe <strong>shaqo doon!</strong></h1>
             <div className="message">
-                <h2>Cauti un loc de munca?</h2>
+                <h2>Cauți un loc de muncă?</h2>
                 <div className="mission">
-                    <h3>Esti in locul potrivit!</h3>
-                    <h3>Misiunea noastra este<br />sa agregam toate locurile de munca inturn singur loc<br />pentru a te ajuta sa gasesti mai usor locul de munca ideal pentru tine!</h3>
+                    <h3>Ești în locul potrivit!</h3>
+                    <h3>Misiunea noastră este<br />să agregăm toate locurile de muncă într-un singur loc<br />pentru a te ajuta să găsești mai ușor locul de muncă ideal pentru tine!</h3>
                 </div>
             </div>
         </div >
